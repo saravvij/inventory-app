12aa98f0e0923cf1bdedc912993ee8d5d71489d7
saravvij@gmail.com <saravvij@gmail.com>
2016-02-24 21:25:43 -0800

    InventoryApp creation

---
+++ app.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+@Component({
+  selector: 'reddit',
+  template: `
+    <div>Hello World!</div>
+  `
+})
+
+class RedditApp   {
+
+    constructor() { }
+}
+
+bootstrap(RedditApp);
